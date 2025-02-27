becfaba44e434f318967d73285d9883ec5f090b0
Sekar S <sekar3s@gmail.com>
2025-02-26 21:46:09 -0600

    Removed Kubernetes Cluster

--- main.tf
+++ main.tf
@@ -31,26 +31,3 @@ data "azurerm_resource_group" "main" {
 #  name     = var.resource_group_name
 #  location = var.location
 #}
-
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
-  dns_prefix          = "exampleaks1"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Production"
-  }
-
-  depends_on = [data.azurerm_resource_group.main]
-}
